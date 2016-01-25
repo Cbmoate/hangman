897d1083c23d9f3d9588e10d8dd0a203502d54e2
CB <cbmoate@gmail.com>
2016-01-25 13:58:08 -0500

    first 2 functions of word.js written for Word constructor

--- word.js
+++ word.js
@@ -1,1 +1,35 @@
+module.exports = Word;
 var Letter = require("./letter.js");
+
+function Word(wrd){
+  this.word = wrd;
+  this.lets = [];
+  this.found = false;
+  getLets: function(){
+    for(var i = 0; i < word.length; i++){
+      this.lets.push(new Letter(wrd[i]));
+    };
+  };
+  checkIfLetterFound: function(guessLetter){
+    var lettersFound = 0;
+    for (var i = this.lets.length - 1; i >= 0; i--) {
+      if(this.lets[i].charac == guessLetter){
+        this.lets[i].appear = true;
+        lettersFound++;
+      };
+    }
+    return lettersFound;
+  };
+  didWeFindTheWOrd: function(){
+    this.lets.every()
+    for (var i = Things.length - 1; i >= 0; i--) {
+      Things[i]
+    };
+  };
+  wordRender: function(){
+    var str = ""
+    for (var i = let.length - 1; i >= 0; i--) {
+      let[i]
+    };
+  };
+}
