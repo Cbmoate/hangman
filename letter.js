e910f4b7b868ab165d3276face77f246dd5249f2
CB <cbmoate@gmail.com>
2016-01-25 13:21:54 -0500

    letter.js fixed up, functioning

--- letter.js
+++ letter.js
@@ -1,13 +1,15 @@
 module.exports = Letter;
+
+//function creates 'Letters' to populate the lets array
 function Letter(let){
   this.appear = false;
   this.charac = let;
   letterRender: function(){
     if(this.appear === false){
-      return("_");
+      return "_";
     }
     else{
-      return(this.charac);
+      return this.charac;
     }
   }
 };
